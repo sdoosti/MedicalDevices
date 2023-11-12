c724d24b0934ea72e87b1b6ee5aaca4bc3fd4043
sdoosti <shahryar65@gmail.com>
2023-11-11 20:47:26 -0800

    create test function for knumer urls

--- test_fda_api.py
+++ test_fda_api.py
@@ -14,3 +14,11 @@ def test_get_pdf_url():
     # Test case 3: Year is less than or equal to 2001
     row = pd.Series({"KNUMBER": "K12345", "date": datetime(2001, 1, 1)})
     assert get_pdf_url(row) == "https://www.accessdata.fda.gov/cdrh_docs/pdf/k12345.pdf"
+
+    # Test case 4: Year is less than 2001
+    row = pd.Series({"KNUMBER": "K12345", "date": datetime(1998, 1, 1)})
+    assert get_pdf_url(row) == "https://www.accessdata.fda.gov/cdrh_docs/pdf/k12345.pdf"
+
+    # Test case: Year is 2005
+    row = pd.Series({"KNUMBER": "K12345", "date": datetime(2005, 1, 1)})
+    assert get_pdf_url(row) == "https://www.accessdata.fda.gov/cdrh_docs/pdf5/k12345.pdf"
